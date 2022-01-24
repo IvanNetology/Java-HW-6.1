5a9e37a3f11e33587f3d3bfd9fee2ef5471736f5
IvanNetology <ivan.ivan.91.91@internet.ru>
2022-01-24 14:25:11 +0300

    add tests

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -2,16 +2,76 @@ package ru.netology.statistic;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StatisticsServiceTest {
 
     @Test
-    void findMax() {
+    void findMaxFirst() {
         StatisticsService service = new StatisticsService();
 
-        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 10};
         long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxLast() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 18};
+        long expected = 18;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxEqual() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7};
+        long expected = 7;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxMinus() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {-77, -34, -18, -30, -97, -10, -3, -64, -4, -25, -7};
+        long expected = -3;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxZero() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {-77, -34, -18, 0, -97, -10, -3, -64, -4, -25, -7};
+        long expected = 0;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxDifferent() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {-77, 34, -18, 0, -97, -10, -3, 64, -4, 25, -7};
+        long expected = 64;
 
         long actual = service.findMax(incomesInBillions);
 
